e83b27dc05206945ca43f2b018c80f7c30fd0df4
Emre Karadeniz <emrekrdenz@gmail.com>
2022-08-21 03:43:25 +0300

    new method added

--- specs/example.spec
+++ specs/example.spec
@@ -10,6 +10,29 @@ Post
 * "user" apiye "post" methoduyla istek at
 * status kod "200" ile ayni mi kontrol et
 
+PostHashmap
+-------------
+* "ad" keyli "emre1" degeri hashmap'e ekle
+* "soyad" keyli "karadeniz1" degeri hashmap'e ekle
+
+* Jobject Oluştur
+* "id" key ve "1" value degerini JObjecte ekle
+* "username" keyine hashmapdeki "ad" keyli değeri JObjecte ekle
+* "firstName" keyine hashmapdeki "soyad" keyli değeri JObjecte ekle
+* "user" apiye "post" methoduyla istek at
+* status kod "200" ile ayni mi kontrol et
+* response "code" keyinin degerini "IDcodee" olarak kaydet
+
+
+* Jobject Oluştur
+* "id" keyine hashmapdeki "IDcodee" keyli değeri JObjecte ekle
+* "username" keyine hashmapdeki "ad" keyli değeri JObjecte ekle
+* "firstName" keyine hashmapdeki "soyad" keyli değeri JObjecte ekle
+* "user" apiye "post" methoduyla istek at
+* status kod "200" ile ayni mi kontrol et
+
+
+
 Get
 -------------
 *Jobject Oluştur
@@ -19,10 +42,29 @@ Get
 * status kod "200" ile ayni mi kontrol et
 * response "type" alanı "unknown" iceriyor mu kontrol et
 
+Get1
+-------------
+* "accept" key "application/json" value degerini headera ekle
+* "api_key" key "emre" value degerini headera ekle
+* Jobject Oluştur
+* "pet/12" apiye "get" methoduyla istek at
+* status kod "200" ile ayni mi kontrol et
+
+spotify
+-------------
+* "Authorization" key "Bearer BQCKP39frqWVpWMHIQ_wOr14QLdqNejhJlWR8Hbk6Hw03reugyLgAEO5qvskT7bhH6i7hOojBnS6V6fYnqMvFCTttETFSYiFX5jMftydxyecgGChrxonqkHVi5BExrrjaFEfhUhrs8bR0BH3C6IO0rI7s-flvHsa7NmeUX5s-lI" value degerini headera ekle
+* "Content-Type" key "application/json" value degerini headera ekle
+* Jobject Oluştur
+* "me" apiye "get" methoduyla istek at
+* status kod "200" ile ayni mi kontrol et
+
 
 Post1
 -------------
-* "Authorization" key "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTk1MjUzNTYsInVzZXJfbmFtZSI6ImVtcmVrYXJhZGVuaXoiLCJhdXRob3JpdGllcyI6WyJST0xFX0NPTVBBTllfQURNSU4iXSwianRpIjoiMTgyOGE3NzYtYmM1My00YzAyLWFlNTctMjVkNWU0NWFlYTA1IiwiY2xpZW50X2lkIjoidGVzdGluaXVtU3VpdGVUcnVzdGVkQ2xpZW50Iiwic2NvcGUiOlsib3BlbmlkIl19.DvrEvlf7Nh2YQshkmxSxhFSFaS7yZUh45NCgidOWcDa2Lupmqw5h5nDnf5xIDJqh_a2y47NH3dVdCO3f4zyXfwlaYsQv07bhqRgHBYdDMxIVYkDwGcblOBFdR7sJnH2uaOhpdGXx_C9GoShbuNmYnVwWF0Y4hIFAAOqVSeqq73OD9AMlHa0DUOe5qi1LpoVXN_-jK_U4Eq91Bpowg60Sh3pDcd1HtzDzTg1xZvAbvji1Cn8lOeg1_2Suo5mpZofBK8psg_RgQqmGN4Iawvp-ZemPIS62mSqYK2hMEnoplKPIH7MvUiBn2GfApRctVJjraJPTtz4mzy-7ZrmabN_gOQ" value degerini headera ekle
+* "Authorization" key "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjEwNTE2OTcsInVzZXJfbmFtZSI6ImVtcmVrYXJhZGVuaXoiLCJhdXRob3JpdGllcyI6WyJST0xFX0NPTVBBTllfQURNSU4iXSwianRpIjoiZjU0MDJmZjEtZDI0Ni00NzhlLWFjMmMtYjQ0YzFjZDc3YmIyIiwiY2xpZW50X2lkIjoidGVzdGluaXVtU3VpdGVUcnVzdGVkQ2xpZW50Iiwic2NvcGUiOlsib3BlbmlkIl19.gc5LmjDhSI3tlF8sqa4HKohh8e5o4VPg_AP61S7YWE0IZU7hJRy2fXe23flz2HFcBgvoKXxXJaKLqR5IhHv3xdWxUttNlsBgw1cGJSMd8-Zor9owX-wCLTYfRfjcN9pvso-4N2rRfEMFcX1l5gfYGWhqkXXJdHmtUfupvPeFYanlILvjxAhiw3UBW0X9fwyWec-vS8pMO3V0xb2FzEDMAfZZF_40aCBDbNQFT-y00rLo3tZVskDiRThi1hJvYZOiY0fBUS2s84kJuGgoa7e0lJoMdk7kM2a0ZAuaLjhXj00m7t_ki5q-DRA_Zj6P2CxLHN0yglMafCahs6sxU_WuZw" value degerini headera ekle
 * Jobject Oluştur
 * "executions" apiye "get" methoduyla istek at
 * status kod "200" ile ayni mi kontrol et
+* response "item_list[0].id" alanı "587548" iceriyor mu kontrol et
+
+
--- src/test/java/org/example/StepImplementation.java
+++ src/test/java/org/example/StepImplementation.java
@@ -4,8 +4,6 @@ import com.thoughtworks.gauge.BeforeScenario;
 import com.thoughtworks.gauge.Step;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.http.Header;
-import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
@@ -17,11 +15,13 @@ import java.util.Map;
 public class StepImplementation {
 
     Map<String, String> headers = new HashMap<>();
+    HashMap<String,Object> hashMap=new HashMap<String,Object>();
 
     JSONObject jObject = null;
     Response response = null;
-    public static final String BASE_URL = "https://petstore.swagger.io/v2/";
-   //public static final String BASE_URL = "https://testinium.io/Testinium.RestApi/api/";
+   // public static final String BASE_URL = "https://api.spotify.com/v1/";
+  // public static final String BASE_URL = "https://testinium.io/Testinium.RestApi/api/";
+   public static final String BASE_URL="https://petstore.swagger.io/v2/";
 
 
     @BeforeScenario
@@ -34,20 +34,21 @@ public class StepImplementation {
     public void createJObject()
     {
         jObject = new JSONObject();
-        System.out.println("Yeni bir JObject Olusturuldu");
+        System.out.print("Yeni bir JObject Olusturuldu");
     }
 
     @Step("<key> key ve <value> value degerini JObjecte ekle")
     public void addToRequestBody(String key, String value)
     {
         jObject.put(key, value);
-        System.out.println("JObject'e " + key + ":" + value + " degeri eklendi");
+        System.out.print("JObject'e " + key + ":" + value + " degeri eklendi");
     }
 
 
     @Step("<api> apiye <type> methoduyla istek at")
     public void setApi(String api,String type)
     {
+        System.out.println(RestAssured.baseURI + RestAssured.basePath + " servisine " + type + " istegi atildi" );
         if(type.equals("post"))
         {
             response= RestAssured.given().headers(headers)
@@ -79,14 +80,16 @@ public class StepImplementation {
         else {
         System.out.println("Lütfen geçerli bir deger giriniz");
         }
+        System.out.println("Request:"+jObject.toString());
         System.out.println("Response:"+response.getBody().asString());
     }
+
 
     @Step("status kod <statusCode> ile ayni mi kontrol et")
     public void checkStatusCode2(int statusCode)
     {
         Assertions.assertEquals(statusCode, response.getStatusCode(), "Status code eslesmiyor...");
-        System.out.println("status kod hata kodu " + response.getStatusCode() + " ile ayni mi kontrol edildi");
+        System.out.print("status kod hata kodu " + response.getStatusCode() + " ile ayni mi kontrol edildi");
     }
 
     @Step("response <key> alanı <value> iceriyor mu kontrol et")
@@ -96,15 +99,56 @@ public class StepImplementation {
         } else {
             Assertions.assertTrue(value.equals(response.jsonPath().get(key).toString()), value + " degeri ile " + response.jsonPath().getString(key) + " degeri uyusmuyor");
         }
-        System.out.println("response " + key + " alani  " + value + " iceriyor mu kontrol edildi");
+        System.out.print("response " + key + " alani  " + value + " iceriyor mu kontrol edildi");
     }
+
+    @Step("response <key> keyinin degerini <hashmapKey> olarak kaydet")
+    public void responseHashmap(String key, String hashmapKey) {
+        hashMap.put(hashmapKey, response.jsonPath().get(key).toString());
+        System.out.println("response " + key + " keyinin degerini " + hashmapKey + " olarak kaydedildi");
+    }
+
+    @Step("<key> keyine hashmapdeki <value> keyli değeri JObjecte ekle")
+    public void AddElementHashmapToRest(String key,String value){
+        jObject.put(key, hashMap.get(value).toString());
+        System.out.print("JObject'e " + key + ":" + hashMap.get(value).toString() + " degeri eklendi");
+    }
+
 
     @Step("<key> key <value> value degerini headera ekle")
     public void addHeader(String key, String value)
     {
         headers.put(key,value);
-        System.out.println("Header'a " +key+ "," +value+ " degeri eklendi");
+        System.out.print("Header'a " +key+ "," +value+ " degeri eklendi");
     }
+
+    @Step("<key> keyli <value> degeri hashmap'e ekle")
+    public void addHashmapManuel(String key, String value)
+    {
+        hashMap.put(key, value);
+        System.out.print(key + " keyli " + value + " degeri manuel olarak hashmap'e eklendi");
+    }
+
+
+    @Step("Hashmapin icindeki <hashmapKey> keyinin degeri <hashmapKey2> keyinin degeri ile <type> mı kontrol et")
+    public void checkDifferenceHashmap(String hashmapKey, String hashmapKey1, String type)
+    {
+        if ("aynı".equals(type))
+        {
+            Assertions.assertEquals(hashMap.get(hashmapKey).toString(), hashMap.get(hashmapKey1).toString(), "hashmapteki degerler eslesiyor...");
+            System.out.println(hashMap.get(hashmapKey).toString()+" , "+ hashMap.get(hashmapKey1).toString() + " ile " + type + "mi kontrol edildi");
+        }
+        else if ("farklı".equals(type))
+        {
+            Assertions.assertNotEquals(hashMap.get(hashmapKey).toString(), hashMap.get(hashmapKey1).toString(), "hashmapteki degerler eslesmiyor...");
+            System.out.println(hashMap.get(hashmapKey).toString()+" , "+ hashMap.get(hashmapKey1).toString() + " ile " + type + " mi kontrol edildi");
+        }
+        else
+        {
+            Assertions.fail("Lütfen Gecerli bir tip giriniz");
+        }
+    }
+
 
 
 
